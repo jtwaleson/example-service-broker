62780c1879fa647e197a264fce480362aed4676b
Jouke Waleson <jouke.waleson@mendix.com>
2017-10-20 12:40:16 +0200

    improve

--- example.py
+++ example.py
@@ -1,11 +1,14 @@
 import uuid
 from router import Service
 from openbrokerapi.catalog import ServiceMetadata, ServicePlan
+import logging
 from openbrokerapi.api import (
     ProvisionedServiceSpec,
     OperationState,
     LastOperation,
     DeprovisionServiceSpec,
+    Binding,
+    UpdateServiceSpec,
 )
 
 
@@ -15,36 +18,41 @@ def gen_uuid(key):
     return str(uuid.uuid5(uuid.NAMESPACE_DNS, key))
 
 
+plans = {
+    'm': ServicePlan(
+        id=gen_uuid('my-unique-plan-sized-m-v0.1'),
+        name='m',
+        description='a medium sized cat, default color: black',
+        metadata=None,
+        free=False,
+        bindable=True,
+    ),
+    's': ServicePlan(
+        id=gen_uuid('my-unique-plan-sized-s-v0.1'),
+        name='s',
+        description='a small sized cat, default color: black',
+        metadata=None,
+        free=False,
+        bindable=True,
+    ),
+}
+
+
+def get_plan_by_id(plan_id):
+    for key, plan in plans.items():
+        if plan.id == plan_id:
+            return key, plan
+    raise KeyError('plan {} not found'.format(plan_id))
+
+
 class ExampleService(Service):
     def __init__(self):
-        plans = []
-        plans.append(
-            ServicePlan(
-                 id=gen_uuid('my-unique-plan-sized-m-v0.1'),
-                 name='m',
-                 description='a medium sized cat, default color: black',
-                 metadata=None,
-                 free=False,
-                 bindable=True,
-            )
-        )
-        plans.append(
-            ServicePlan(
-                 id=gen_uuid('my-unique-plan-sized-s-v0.1'),
-                 name='s',
-                 description='a small sized cat, default color: black',
-                 metadata=None,
-                 free=False,
-                 bindable=True,
-            )
-        )
-
         super().__init__(
             id=gen_uuid('my-very-unique-service'),
             name='example-service',
             description='Example Service does nothing',
             bindable=True,
-            plans=plans,
+            plans=plans.keys(),
             tags=['example', 'service'],
             requires=None,
             metadata=ServiceMetadata(
@@ -63,27 +71,46 @@ class ExampleService(Service):
         return True  # for now the ServiceBrokerRouter only supports one service
 
     def provision(self, instance_id, service_details, async_allowed):
+        key, plan = get_plan_by_id(service_details.plan_id)
+        logging.info('creating plan: {} - {}'.format(key, str(plan)))
         return ProvisionedServiceSpec(
             is_async=True, dashboard_url=None, operation='create',
         )
+        # :raises ErrInstanceAlreadyExists: If instance already exists
+        # :raises ErrAsyncRequired: If async is required but not supported
         # raise NotImplementedError('sorry, not yet implemented')
 
     def bind(self, instance_id, binding_id, details):
-        raise NotImplementedError
+        key, plan = get_plan_by_id(details.plan_id)
+        logging.info('binding plan: {} - {}'.format(key, str(plan)))
+        return Binding(
+            credentials={
+                'plan_id': details.plan_id,
+                'parameters': str(details.parameters),
+            },
+        )
+        # :raises ErrBindingAlreadyExists: If binding already exists
+        # :raises ErrAppGuidNotProvided: If AppGuid is required but not provided
         # return Binding()
 
     def update(self, instance_id, details, async_allowed):
         raise NotImplementedError
-        # return UpdateServiceSpec()
+        # cases:
+        # - can not update: plan incompatible
+        # - can not update: backed returned error
+        # - no params: send current status and supported methods??
+        # - success
+        # :raises ErrAsyncRequired: If async is required but not supported
+        return UpdateServiceSpec(is_async=True, operation='update')
 
     def unbind(self, instance_id, binding_id, details):
-        raise NotImplementedError
+        pass
 
     def deprovision(self, instance_id, details, async_allowed):
         if True:  # async
-            return DeprovisionServiceSpec(True, 'delete')
+            return DeprovisionServiceSpec(is_async=True, operation='delete')
         else:
-            return DeprovisionServiceSpec(False)
+            return DeprovisionServiceSpec(is_async=False)
 
     def last_operation(self, instance_id, operation_data):
         if operation_data == 'create':
@@ -92,5 +119,8 @@ class ExampleService(Service):
         elif operation_data == 'delete':
             # check if deletion was successful
             return LastOperation(OperationState.SUCCEEDED, 'deleted')
+        elif operation_data == 'update':
+            # check if deletion was successful
+            return LastOperation(OperationState.SUCCEEDED, 'updated')
         else:
             return LastOperation(OperationState.FAILED, 'failed, no valid operation id found')
